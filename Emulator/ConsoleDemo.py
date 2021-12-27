dec9e27c512f8a4bd48c55581d27613717d4365b
Halid Yıldırım <48795375+SMDHuman@users.noreply.github.com>
2021-12-27 21:00:49 +0300

    Haftalık Kafa Açıcı 2.Kısım
    Haftalık Kafa Açıcı serimizin 2.kısmında yaptığımız yenilikler

--- Emulator/ConsoleDemo.py
+++ Emulator/ConsoleDemo.py
@@ -1,16 +1,25 @@
-import ConsoleAPI as cs 
+import ConsoleAPI_EMU as cs 
 import time
 
 win = cs.LCD(3)
 
 win.pixel(1,1)
-win.pixel(0,0, "red")
+win.pixel(0,0, (1, 0, 0))
 
-win.pixel(4, 4, "blue")
-win.pixel(45, 4, "blue")
-win.rectangle(5, 5, 40, 40, outLine="red", inFill = "green",  outLineWidth = 4)
+for i in range(0, 12, 2):
+	win.pixel(4 + i, 4, "white")
+	win.pixel(5 + i, 4, "grey")
+win.rectangle(5, 5, 10, 10, outLine=(1, 0, 0), inFill = (0, 0, 1))
 
-win.line(30, 50, 100, 100, "gray")
+
+print(win.getPixel(7, 7))
+
+win.circle(50, 50, 14, outLine="red", outLineWidth = 1, inFill = "yellow")
+win.update()
+points = [[100, 100], [150, 100], [150, 150], [125, 140], [120, 150]]
+win.polygon(points)
+
+win.bucketFill(120, 120, "brown")
 
 while(1):
 	win.update()
