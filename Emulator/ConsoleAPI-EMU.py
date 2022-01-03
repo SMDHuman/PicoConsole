dadbe55988e8dac7afe6dee2d14274ea0970adaf
Halid Yıldırım <48795375+SMDHuman@users.noreply.github.com>
2022-01-03 20:46:13 +0300

    Create ConsoleAPI-EMU.py

--- Emulator/ConsoleAPI-EMU.py
+++ Emulator/ConsoleAPI-EMU.py
@@ -1,5 +1,6 @@
 from graphics import *
 import keyboard, webcolors
+from itertools import permutations
 
 class LCD:
 	def __init__(self, scale = 1):
@@ -42,7 +43,7 @@ class LCD:
 		dx, dy = (x2 - x1), (y2 - y1)
 		step = [abs(dy), abs(dx)][abs(dx) >= abs(dy)]
 		dx, dy = dx / step, dy / step
-		for i in range(1, step + 1):
+		for i in range(step):
 			self.pixel(x1, y1)
 			x1 += dx
 			y1 += dy
@@ -53,28 +54,38 @@ class LCD:
 		return(color)
 
 	def _drawCircle(self, xc, yc, r, color, outLineWidth):
-		x, y = 0, r
-		d = 3 - 2 * r
-		self._drawCirclePixel(xc, yc, x, y, color, outLineWidth)
-		while(y >= x):
-			x+=1
-			if (d > 0):
-				y-=1
-				d = d + 4 * (x - y) + 10
-			else:
-				d = d + 4 * x + 6
-			self._drawCirclePixel(xc, yc, x, y, color, outLineWidth)
+		rs = r*(r-1)
+		for y2 in range(yc - r, yc + r + 1):
+			for x2 in range(xc - r, xc + r + 1):
+				d = (x2 - xc)**2 + (y2 - yc)**2
+				if(rs + r >= d >= rs - r*outLineWidth):
+					self.pixel(x2, y2, color)
 
-	def _drawCirclePixel(self, xc, yc, x, y, color, outLineWidth):
-		for d in range(0, outLineWidth):
-			self.pixel(xc+x, yc+y - d, color)
-			self.pixel(xc-x, yc+y - d, color)
-			self.pixel(xc+x, yc-y + d, color)
-			self.pixel(xc-x, yc-y + d, color)
-			self.pixel(xc+y - d, yc+x, color)
-			self.pixel(xc+y - d, yc-x, color)
-			self.pixel(xc-y + d, yc+x, color)
-			self.pixel(xc-y + d, yc-x, color)
+	def _circleFill(self, xc, yc, r, color, outLineWidth):
+		rs = r*(r-outLineWidth)
+		for y2 in range(yc - r, yc + r):
+			for x2 in range(xc - r, xc + r):
+				x0, y0 = x2 - xc, y2 - yc
+				if(x0**2 + y0**2 < rs):
+					self.pixel(x2, y2, color)
+
+	def _areaTri(self, tri):
+		return(abs(tri[0][0]*tri[1][1] + tri[1][0]*tri[2][1] + tri[2][0]*tri[0][1] - tri[0][0]*tri[2][1] - tri[1][0]*tri[0][1] - tri[2][0]*tri[1][1])/2)
+
+	def _drawTriagnle(self, points, color):
+		tArea = self._areaTri(points)
+		xs = [p[0] for p in points]
+		ys = [p[1] for p in points]
+		minX = min(xs)
+		maxX = max(xs)
+		minY = min(ys)
+		maxY = max(ys)
+		for y in range(minY, maxY+1):
+			for x in range(minX, maxX+1):
+				ds = [self._areaTri([[x, y], t[0], t[1]]) for t in permutations(points, 2)]
+				#print(sum(ds), tArea)
+				if(sum(ds)/2 == tArea):
+					self.pixel(x, y, color)
 
 	def getPixel(self, x, y):
 		# İnstenilen noktanın renk değerini döndürür
@@ -122,21 +133,29 @@ class LCD:
 
 		for pixel in pixels:
 			self.pixel(pixel[0], pixel[1], color)
-			if(self.getPixel(pixel[0] + 1, pixel[1]) == mainColor and not([pixel[0] + 1, pixel[1]] in pixels) and pixel[0] + 1 < self.winX):
-				pixels.append([pixel[0] + 1, pixel[1]])
-			if(self.getPixel(pixel[0] - 1, pixel[1]) == mainColor and not([pixel[0] - 1, pixel[1]] in pixels) and pixel[0] - 1 >= 0):
-				pixels.append([pixel[0] - 1, pixel[1]])
-			if(self.getPixel(pixel[0], pixel[1] + 1) == mainColor and not([pixel[0], pixel[1] + 1] in pixels) and pixel[1] + 1 < self.winY):
-				pixels.append([pixel[0], pixel[1] + 1])
-			if(self.getPixel(pixel[0], pixel[1] - 1) == mainColor and not([pixel[0], pixel[1] - 1] in pixels) and pixel[1] - 1 >= 0):
-				pixels.append([pixel[0], pixel[1] - 1])
+			if(not([pixel[0] + 1, pixel[1]] in pixels)):
+				if(pixel[0] + 1 < self.winX):
+					if(self.getPixel(pixel[0] + 1, pixel[1]) == mainColor):
+						pixels.append([pixel[0] + 1, pixel[1]])
+			if(not([pixel[0] - 1, pixel[1]] in pixels)):
+				if(pixel[0] - 1 >= 0):
+					if(self.getPixel(pixel[0] - 1, pixel[1]) == mainColor):
+						pixels.append([pixel[0] - 1, pixel[1]])
+			if(not([pixel[0], pixel[1] + 1] in pixels)):
+				if(pixel[1] + 1 < self.winY):
+					if(self.getPixel(pixel[0], pixel[1] + 1) == mainColor):
+						pixels.append([pixel[0], pixel[1] + 1])
+			if(not([pixel[0], pixel[1] - 1] in pixels)):
+				if(pixel[1] - 1 >= 0):
+					if(self.getPixel(pixel[0], pixel[1] - 1) == mainColor):
+						pixels.append([pixel[0], pixel[1] - 1])
 		return(0)
 
 	def circle(self, xc, yc, r, outLine = "white", inFill = "black", outLineWidth = 1):
 		outLine = self._color(outLine)
 		inFill = self._color(inFill)
+		self._circleFill(xc, yc, r, inFill, outLineWidth)
 		self._drawCircle(xc, yc, r, outLine, outLineWidth)
-		self.bucketFill(xc, yc, inFill)
 		return(0)
 
 	def update(self):
@@ -162,10 +181,14 @@ class LCD:
 		# Ekran parlaklığını değiştir
 		return(0)
 
-	def polygon(self, points):
+	def polygon(self, points, outLine = "white", inFill = "black"):
 		# Verilen boktalar arasında çizgi çekerek çokgen oluşturma
+		triangles = [[points[0], points[i + 1], points[i + 2]] for i in range(len(points) - 2)]
+		for triangle in triangles:
+			self._drawTriagnle(triangle, inFill)
+
 		for i in range(len(points)):
-			self.line(points[i-1][0], points[i-1][1], points[i][0], points[i][1])
+			self.line(points[i-1][0], points[i-1][1], points[i][0], points[i][1], outLine)
 
 class Inputs:
 	def __init__(self):
